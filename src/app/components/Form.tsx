93598f2bb30ad9b61f506258db2ca58e7c6e1269
Ilham Firmansyah <milhamfirmansyah5@gmail.com>
2024-02-12 13:29:59 +0700

    change type input to select input

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -5,7 +5,16 @@ function Form() {
     <form action="#" className="form-container">
       <div className="form-input-container">
         <label htmlFor="">Select Service</label>
-        <input type="text" placeholder="Dental Care" />
+        <select name="Service" id="" style={{ padding: '17.75px 24px' }}>
+          <option value="Primary Care">Primary Care</option>
+          <option value="Dermatology">Dermatology</option>
+          <option value="Psychiatrist">Psychiatrist</option>
+          <option value="Sport Medicine">Sport Medicine</option>
+          <option value="Neurology">Neurology</option>
+          <option value="Cardiology">Cardiology</option>
+          <option value="Maternity">Maternity</option>
+          <option value="Dental Care">Dental Care</option>
+        </select>
       </div>
       <div className="form-input-container">
         <label htmlFor="">Select Location</label>
@@ -15,7 +24,9 @@ function Form() {
         <label htmlFor="">Select Date</label>
         <input type="text" placeholder="2024" />
       </div>
-      <button className="button-primary form-button" style={{padding: '18px 27px'}}>Make An Appointment</button>
+      <button className="button-primary form-button" style={{ padding: '18px 27px' }}>
+        Make An Appointment
+      </button>
     </form>
   );
 }
